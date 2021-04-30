d0c575013d0bf9ec514050a5cfa76e2a39f2dae1
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2021-04-30 07:46:15 -0600

    React Added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Agile Monkey Project Carlos Artiles
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
