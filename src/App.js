d0a7ea4459269ffcc3d55b06457358eeed74550c
cartiles0 <61708300+cartiles0@users.noreply.github.com>
2021-05-05 14:50:54 -0600

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
-import Body from "./pages/Body";
+import Body from "./pages/";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Body />
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import 'react-tabs/style/react-tabs.css';
+import 'bulma/bulma.sass'
+
+function Header() {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+          <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--2PbnD5oV--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/organization/profile_image/645/69ecc7e6-e64b-4230-928e-564df2008cea.png" alt="The Agile Monkeys Logo"/>
+          </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
